7cce4941dfa043d573a3c50c194a847cd569fc4b
BuildTools <jgregory2222@gmail.com>
2017-01-29 21:50:44 -0600

    Initial commit

---
+++ FormExample/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace FormExample
+{
+    public partial class Form1 : Form
+    {
+        public static Form form;
+        public static Thread thread;
+        public static double s = 0;
+        public static int fps = 60;
+        public static double running_fps = 60.0;
+        public static Sprite sprite;
+        public static long desired = 0;
+        public static Image brando = Image.FromFile("brando.png");
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            form = this;
+            thread = new Thread(new ThreadStart(run));
+            thread.Start();
+            sprite = new Sprite(ClientSize.Height - 20, ClientSize.Width - 20);
+
+        }
+
+        public static void run()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                running_fps = .9 * running_fps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                Console.WriteLine(running_fps);
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds< frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime-diff).Milliseconds);
+                last = DateTime.Now;
+                BrandoSwarm();
+                s += 1.0 / running_fps;
+                form.Invoke(new MethodInvoker(form.Refresh));
+                
+            }
+        }
+
+        private void UpdateSize()
+        {
+            sprite.setDimensions(ClientSize.Height - 20, ClientSize.Width - 20);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            thread.Abort();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+
+            
+            Refresh();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            //Refresh();
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (e.KeyChar == 'a' || e.KeyChar == '0' || e.KeyChar == '1') 
+            {
+                desired += 1;
+            }
+            else if (e.KeyChar == 'r')
+            {
+                if (desired > 0)
+                {
+                    desired -= 1;
+                }
+            }
+            else if (e.KeyChar == 'c')
+            {
+                sprite.clear();
+                desired = 0;
+            }
+            else if (e.KeyChar == '2') 
+            {
+                desired += 10;
+            }
+            else if (e.KeyChar == '3')
+            {
+                desired += 100;
+            }
+            else if (isDigit(e.KeyChar))
+            {
+                desired += 10 * Int32.Parse(e.KeyChar+"");
+            }
+            else
+            {
+                Random rand = new Random();
+                desired += rand.Next(1, 101);
+            }
+        }
+
+        protected Boolean isDigit(char c)
+        {
+            return (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9');
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            //Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Console.WriteLine(s);
+            sprite.render(e.Graphics);
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(200, Color.Black)), 0, 0, ClientSize.Width, 20);
+            String s1 = "Brando Count: " + sprite.count();
+            int s1x = 5;
+            e.Graphics.DrawString(s1, new Font("Comic Sans MS", 10), Brushes.LawnGreen, s1x, 0);
+            String s2 = "FPS: " + running_fps;
+            int s2x = 200;
+            e.Graphics.DrawString(s2, new Font("Comic Sans MS", 10), Brushes.LawnGreen, s2x, 0);
+            double adjSpeed = 1.0 / running_fps;
+            String s3 = "Speed per tick: " + adjSpeed;
+            int s3x = 400;
+            e.Graphics.DrawString(s3, new Font("Comic Sans MS", 10), Brushes.LawnGreen, s3x, 0);
+            String s4 = "Speed per second: " + (running_fps * adjSpeed);
+            int s4x = 650;
+            e.Graphics.DrawString(s4, new Font("Comic Sans MS", 10), Brushes.LawnGreen, s4x, 0);
+            float v = (float)(Math.Abs(200 + 100 * Math.Sin((s * 2 / Math.PI))));
+            e.Graphics.DrawRectangle(new Pen(Color.Navy, 10), 0, 0, v, v);
+
+        }
+
+        protected static void BrandoSwarm()
+        {
+            while (sprite.count() < desired)
+            {
+                sprite.add(new Face(form.ClientSize.Height - 20, form.ClientSize.Width - 20));
+            }
+            while(sprite.count() > desired)
+            {
+                if (sprite.count() <= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    sprite.remove();
+                }
+            }
+        }
+
+
+    }
+    
+}
---
+++ FormExample/Sprite.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace FormExample
+{
+    public class Sprite
+    {
+        //instance variable
+        private float x=0;
+        protected int height=100;
+        protected int width=100;
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private float y = 0;
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        private float scale = 1;
+
+        public float Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        protected List<Sprite> children = new List<Sprite>();
+
+
+        //constructors
+
+        public Sprite(int h, int w)
+        {
+            height = h;
+            width = w;
+        }
+
+        public Sprite()
+        {
+        }
+
+        //methods
+        public void render(Graphics g)
+        {
+            Matrix original = g.Transform.Clone();
+            g.ScaleTransform(scale,scale);
+            g.TranslateTransform(x, y);
+            paint(g);
+            foreach(Sprite s in children)
+            {
+                s.render(g);
+            }
+            g.Transform = original;
+        }
+
+        public void setDimensions(int h, int w)
+        {
+            height = h;
+            width = w;
+            foreach (Sprite s in children)
+            {
+                s.setDimensions(height, width);
+            }
+        }
+
+        public void clear()
+        {
+            children.Clear();
+        }
+
+        public virtual void paint(Graphics g)
+        {
+
+        }
+
+        public void add(Sprite s)
+        {
+            children.Add(s);
+        }
+
+        public void remove()
+        {
+            children.Remove(children.Last());
+        }
+
+        public long count()
+        {
+            return children.Count;
+        }
+
+    }
+
+    public class Face :Sprite
+    {
+        private Image breading = Image.FromFile("brando.png");
+        private int hseed = 0;
+        private int wseed = 0;
+        private int hspeed = 1;
+        private int wspeed = 1;
+        private int s = 0;
+        private int type;
+
+        public Face(int h, int w) : base(h, w)
+        {
+            Random rand = new Random();
+            hseed = rand.Next(500) + DateTime.Now.Millisecond % h;
+            wseed = rand.Next(500) + DateTime.Now.Millisecond % w;
+            hspeed = rand.Next(-5,5);
+            wspeed = rand.Next(-5,5);
+            Random randobrando = new Random();
+            type = randobrando.Next(4);
+        }
+        
+        public override void paint(Graphics g)
+        {
+            float x = 0;
+            float y = 0;
+            if (type == 0)
+            {
+                x = (float)(((width - 75) / 2) * Math.Cos((s / 50.0) + wspeed + wseed)) + ((width - 75) / 2);
+                y = (float)(((height - 75) / 2) * Math.Sin((s / 50.0) + hspeed + hseed)) + ((height - 75) / 2);
+            }
+            else if (type == 1)
+            {
+                x = (float)(((width - 75) / 2) * Math.Sin((s / 50.0) + wspeed + wseed)) + ((width - 75) / 2);
+                y = (float)(((height - 75) / 2) * Math.Cos((s / 50.0) + hspeed + hseed)) + ((height - 75) / 2);
+            }
+            else if (type == 2)
+            {
+                x = (float)(((width - 75) / 2) * Math.Cos((s / 50.0) + wspeed + wseed)) + ((width - 75) / 2);
+                y = (float)(((height - 75) / 2) * Math.Cos((s / 50.0) + hspeed + hseed)) + ((height - 75) / 2);
+            }
+            else if (type == 3)
+            {
+                x = (float)(((width - 75) / 2) * Math.Sin((s / 50.0) + wspeed + wseed)) + ((width - 75) / 2);
+                y = (float)(((height - 75) / 2) * Math.Sin((s / 50.0) + hspeed + hseed)) + ((height - 75) / 2);
+            }
+            g.DrawImage(breading, x, y);
+            s++;
+        }
+
+    }
+}
